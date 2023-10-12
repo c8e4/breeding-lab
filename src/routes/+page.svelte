24cc8f109be56cf64906a7bae4ab179f6be15d45
c8e4 <c8e4@proton.me>
2023-10-12 18:28:54 +0200

    view combinations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,85 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { store } from "$lib/store";
+
+    function onPaste(event) {
+        event.preventDefault();
+        let paste = (event.clipboardData || window.clipboardData).getData(
+            "text"
+        );
+        console.log(paste);
+    }
+
+    let fragmentGroups = [{ text: "giganose" }, { text: "mask" }];
+    let samples = [];
+
+    let showGroups = true;
+    function toggleGroups() {
+        showGroups = !showGroups;
+    }
+    function calculate() {
+        samples = [];
+        let candidates = fragmentGroups.map((g) =>
+            store.fragments.filter((f) => f.tags.includes(g.text))
+        );
+        for (let i = 0; i < candidates[0].length; i++) {
+            for (let j = 0; j < candidates[1].length; j++) {
+                samples.push(
+                    [
+                        ...candidates[0][i].layers,
+                        ...candidates[1][j].layers,
+                    ].sort((a, b) => a.order - b.order)
+                );
+            }
+        }
+    }
+</script>
+
+<svelte:window on:paste={onPaste} />
+<div>
+    <button on:click={toggleGroups}>
+        {#if showGroups}
+            -
+        {:else}
+            +
+        {/if}
+        groups</button
+    >
+</div>
+
+{#if showGroups}
+    <div class="flex flex-col">
+        {#each fragmentGroups as group}
+            <textarea type="text" value={group.text} />
+        {/each}
+    </div>
+    <div><button on:click={calculate}>calculate</button></div>
+{/if}
+
+<div class="w-full flex flex-wrap">
+    {#each samples as layerlist}
+        <div class="relative" style="width:100px;height:200px">
+            {#each layerlist as layer}
+                {#if layer.change}
+                    <img
+                        class="absolute"
+                        style="width:100px;height:200px;"
+                        src="bitmasks/export/{layer.change}"
+                        alt=""
+                    />
+                {/if}
+                <img
+                    class="absolute"
+                    style="width:100px;height:200px;"
+                    src="bitmasks/export/{layer.outline}"
+                    alt=""
+                />
+            {/each}
+        </div>
+    {/each}
+</div>
+
+<style>
+    textarea {
+        border: 1px solid black;
+    }
+</style>
